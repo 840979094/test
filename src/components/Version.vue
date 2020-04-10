d3bd93a082808df3d746acdb6a4e34d312fc6f77
heaven <840979094@qq.com>
2020-04-10 22:03:09 +0800

    这是version的第一天

---
+++ src/components/Version.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    {{arr}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        arr: [1, 2, 3, 4, 5]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
